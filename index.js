1d42cf8d05dc1e45d27bd11ae87f51912f9712da
Elle Oshiro <ekoshiro16@gmail.com>
2023-03-02 23:17:20 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+/*
+You are a hardworking and obedient worker for a commercial space travel company, and your life is going great! Or so you thought... Turns out, your supervisor has just told you that there is an imposter among us at the company! Lucky for you (and unluckily for the imposter), you are a master programmer who knows the perfect algorithm to fix this situation.
+
+Write a function called findImposter that takes in one argument, which you can expect will always be an array of some numbers, where each number represents a valid employee credential. All of the numbers in the array will be the same, except for one. That one unique number represents an invalid employee credential. 
+
+Your function should return that one "imposter" number. 
+
+For example:
+When given an argument array like this: 
+
+[1, 1, 1, 1, 2, 1, 1]
+
+Your function should return 2, because it is the one unique, imposter number. 
+
+Or when given an argument array like this:
+
+[42, 42, 42, 64, 42, 42, 42]
+
+Your function should return 64, because it is the one unique, imposter number. 
+
+NOTES:
+You can always expect that the input argument array will have at least 3 elements.
+You can always expect that the input array argument will have 1 unique number. 
+*/
+
+// Write your code below: 
